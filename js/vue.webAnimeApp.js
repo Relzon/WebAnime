af48456a8cbe002744d98605089a94c32a7cd7cb
Relzon <chamomile@angeldev.org>
2021-07-21 18:46:03 +0300

    Добавление комментариев

--- js/vue.webAnimeApp.js
+++ js/vue.webAnimeApp.js
@@ -6,33 +6,33 @@
 
 const HeaderOptions = {
     data: () => ({
-        /* Парамеры лого */
+        /* Параметры лого */
         logoStyleClass: 'header_logo', // Класс стлей для логотипа
         logoLink: 'images/logo.png', // Ссылка на логотип
 
         /* Параметры кнопки прокрутки */
         upScrollButtonExistence: false, // Существование кнопки прокрутки
-        heightForButtonAppear: 100, // Дистанция от верхнего края окна браузера, при превышении которой кнопка становится видимой
+        heightForButtonAppear: 100, // Дистанция от верхнего края окна браузера, при превышении которой появляется кнопка прокрутки
         heightForScrollUpTo: 0, // Дистанция от верхнего края окна браузера к которой будет прокручена страница
         upScrollButtonId: 'move_up', // ID кнопки
         upScrollButtonText: 'Наверх' // Текст на кнопке
     }),
     created () {
-        window.addEventListener('scroll', this.onScroll)
+        window.addEventListener('scroll', this.onScroll) // Добавление слушателя события
     },
     destroyed () {
-        window.removeEventListener('scroll', this.onScroll)
+        window.removeEventListener('scroll', this.onScroll) // Удаление слушателя события
     },
     methods: {
-        onScroll() {
+        onScroll() { // Событие прокрутки страницы
             if (window.scrollY > this.heightForButtonAppear) {
-                this.upScrollButtonExistence = true
+                this.upScrollButtonExistence = true // Кнопка прокрутки существует
             } else {
-                this.upScrollButtonExistence = false
+                this.upScrollButtonExistence = false // Кнопка прокрутки НЕ существует
             }
         },
-        upScroll() {
-            window.scrollTo(0, this.heightForScrollUpTo)
+        upScroll() { // Событие прокрутки вверх (при нажатии на кнопку)
+            window.scrollTo(0, this.heightForScrollUpTo) // Прокрутка страницы
         }
     }
 }
